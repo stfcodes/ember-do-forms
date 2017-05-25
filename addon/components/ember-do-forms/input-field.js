27d9ce6b1a62b614b926a8ff38ccafb9fef92f7b
shuriu <stefan@kuibul.ro>
2017-05-25 15:43:33 +0300

    refactor(input-field): Extract complex field functionality into a mixin
    Also removes the unused labelComponent property.

--- addon/components/ember-do-forms/input-field.js
+++ addon/components/ember-do-forms/input-field.js
@@ -1,44 +1,24 @@
 import Ember from 'ember';
 import layout from '../../templates/components/ember-do-forms/input-field';
 import setDataTestSelector from '../../utils/set-data-test-selector';
+import ComplexFieldMixin from '../../mixins/complex-field';
 
 const {
   assert,
   Component,
   get,
-  isPresent,
-  isEmpty,
-  inject: { service },
-  set
+  isPresent
 } = Ember;
 
-const InputFieldComponent = Component.extend({
+const InputFieldComponent = Component.extend(ComplexFieldMixin, {
   layout,
   tagName: '',
 
-  config: service('ember-do-forms/config'),
-
   controlType: 'text',
-
-  labelComponent: 'do-label',
-
-  labelClasses: [],
-  controlClasses: [],
-  feedbackClasses: [],
-  hintClasses: [],
 
   init() {
     this._setTestSelectors();
 
-    this._super(...arguments);
-
-    assert('{{ember-do-forms/input-field}} requires an object to be passed in', isPresent(get(this, 'object')));
-    assert('{{ember-do-forms/input-field}} requires a propertyName to be passed in', isPresent(get(this, 'propertyName')));
-
-    this._setChildClasses();
-  },
-
-  _setTestSelectors() {
     // FIXME: data-test-input-field should be removed in next
     // major version as it adds an unnecessary data-test-* attribute
     setDataTestSelector(this, {
@@ -48,60 +28,12 @@ const InputFieldComponent = Component.extend({
       testSelectorValue: get(this, 'propertyName')
     });
 
-    setDataTestSelector(this, {
-      testSelector: 'do-field',
-      testSelectorProperty: '_dataTestSelectorField',
-      autoTestSelector: get(this, 'config.autoDataTestSelectors'),
-      testSelectorValue: get(this, 'propertyName')
-    });
+    this._super(...arguments);
 
-    setDataTestSelector(this, {
-      testSelector: 'do-label',
-      testSelectorProperty: '_dataTestSelectorLabel',
-      autoTestSelector: get(this, 'config.autoDataTestSelectors'),
-      testSelectorValue: get(this, 'propertyName')
-    });
+    assert('{{ember-do-forms/input-field}} requires an object to be passed in', isPresent(get(this, 'object')));
+    assert('{{ember-do-forms/input-field}} requires a propertyName to be passed in', isPresent(get(this, 'propertyName')));
 
-    setDataTestSelector(this, {
-      testSelector: 'do-control',
-      testSelectorProperty: '_dataTestSelectorControl',
-      autoTestSelector: get(this, 'config.autoDataTestSelectors'),
-      testSelectorValue: get(this, 'propertyName')
-    });
-
-    setDataTestSelector(this, {
-      testSelector: 'do-feedback',
-      testSelectorProperty: '_dataTestSelectorFeedback',
-      autoTestSelector: get(this, 'config.autoDataTestSelectors'),
-      testSelectorValue: get(this, 'propertyName')
-    });
-
-    setDataTestSelector(this, {
-      testSelector: 'do-hint',
-      testSelectorProperty: '_dataTestSelectorHint',
-      autoTestSelector: get(this, 'config.autoDataTestSelectors'),
-      testSelectorValue: get(this, 'propertyName')
-    });
-  },
-
-  _setChildClasses() {
-    let defaultClasses = get(this, 'config.defaultClasses');
-
-    if (isEmpty(get(this, 'labelClasses'))) {
-      set(this, 'labelClasses', defaultClasses.label);
-    }
-
-    if (isEmpty(get(this, 'controlClasses'))) {
-      set(this, 'controlClasses', defaultClasses.control);
-    }
-
-    if (isEmpty(get(this, 'feedbackClasses'))) {
-      set(this, 'feedbackClasses', defaultClasses.feedback);
-    }
-
-    if (isEmpty(get(this, 'hintClasses'))) {
-      set(this, 'hintClasses', defaultClasses.hint);
-    }
+    this._setChildClasses();
   }
 });
 
