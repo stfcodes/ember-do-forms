d11d410714f47459066602758f7a372a3e976e73
mjuniper <mjuniper@gmail.com>
2020-01-30 13:00:21 -0700

    tweaking initializer

--- addon/instance-initializers/ember-do-forms/initializer.js
+++ addon/instance-initializers/ember-do-forms/initializer.js
@@ -3,13 +3,16 @@ import { set } from '@ember/object';
 import configDefaults from '../../utils/config-defaults';
 
 export function initialize(applicationInstance) {
+  debugger;
   const config = applicationInstance.resolveRegistration('config:environment')['ember-do-forms'];
-  let doFormConfig  = assign(configDefaults(), config);
-  let configService = applicationInstance.lookup('service:ember-do-forms/config');
+  if (config || applicationInstance.router) {
+    let doFormConfig  = assign(configDefaults(), config);
+    let configService = applicationInstance.lookup('service:ember-do-forms/config');
 
-  Object.keys(doFormConfig).forEach((key) => {
-    set(configService, key, doFormConfig[key]);
-  });
+    Object.keys(doFormConfig).forEach((key) => {
+      set(configService, key, doFormConfig[key]);
+    });
+  }
 }
 
 export default {
