33937f3b1611c64eb37c54f257369cde73618c6d
mjuniper <mjuniper@gmail.com>
2020-01-31 12:19:19 -0700

    merge => assign

--- addon/instance-initializers/ember-do-forms/initializer.js
+++ addon/instance-initializers/ember-do-forms/initializer.js
@@ -1,10 +1,10 @@
-import { merge } from '@ember/polyfills';
+import { assign } from '@ember/polyfills';
 import { set } from '@ember/object';
 import config from 'ember-get-config';
 import configDefaults from '../../utils/config-defaults';
 
 export function initialize(appInstance) {
-  const doFormConfig  = merge(configDefaults(), config['ember-do-forms']);
+  const doFormConfig  = assign(configDefaults(), config['ember-do-forms']);
   const configService = appInstance.lookup('service:ember-do-forms/config');
 
   Object.keys(doFormConfig).forEach((key) => {
